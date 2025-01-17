05b913d799c5f425e15c1bf6d6c7cd0b30aa75cc
Theor52 <theor.1.137@gmail.com>
2025-01-17 18:17:06 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // створення чисел різних типів
+      
         int intNumber = 42;
         double doubleNumber = 42.58;
         float floatNumber = 19.95f;
@@ -10,7 +10,7 @@ public class Main {
         short shortNumber = 32000;
         byte byteNumber = 127;
 
-        // долучення чисел до списку (універсальний список)
+       
         List<Number> numbers = new ArrayList<>();
         numbers.add(intNumber);
         numbers.add(doubleNumber);
@@ -19,25 +19,25 @@ public class Main {
         numbers.add(shortNumber);
         numbers.add(byteNumber);
 
-        // виведення чисел на екран
+        
         System.out.println("All numbers:");
         for (Number number : numbers) {
             System.out.println(number);
         }
 
-        // виведення чисел у форматі цілих чисел
+        
         System.out.println("\nNumbers as integers:");
         for (Number number : numbers) {
             System.out.println(number.intValue());
         }
 
-        // виведення чисел у форматі дробних чисел з 2 знаками після коми
+        
         System.out.println("\nNumbers as floats with 2 decimal places:");
         for (Number number : numbers) {
             System.out.printf("%.2f\n", number.doubleValue());
         }
 
-        // розподіл чисел за типами в окремі списки
+       
         List<Integer> integerList = new ArrayList<>();
         List<Double> doubleList = new ArrayList<>();
         List<Float> floatList = new ArrayList<>();
@@ -61,7 +61,7 @@ public class Main {
             }
         }
 
-        // виведення чисел з кожного списку
+       
         System.out.println("\nIntegers: " + integerList);
         System.out.println("Doubles: " + doubleList);
         System.out.println("Floats: " + floatList);
@@ -69,10 +69,10 @@ public class Main {
         System.out.println("Shorts: " + shortList);
         System.out.println("Bytes: " + byteList);
 
-        // обчислення суми квадратів всіх чисел
+     
         double sumOfSquares = 0;
         for (Number number : numbers) {
-            // зведення до квадрату
+         
             sumOfSquares += Math.pow(number.doubleValue(), 2);
         }
         System.out.println("\nSum of squares of all numbers: " + sumOfSquares);
